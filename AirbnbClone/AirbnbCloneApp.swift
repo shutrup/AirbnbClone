4e6d13c282f4dcdeb74c19215c0921c5769a9660
shutrup <magagaraev95@mail.ru>
2023-10-25 19:11:29 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Шарап Бамматов on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
